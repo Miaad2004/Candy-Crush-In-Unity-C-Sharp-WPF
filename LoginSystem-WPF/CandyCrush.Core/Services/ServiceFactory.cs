c4f02a9be79672334ee6810a1f5af4841930f347
Miaad <Miaad@MIAAD>
2023-06-27 10:21:59 +0330

    added ServiceFactory

---
+++ LoginSystem-WPF/CandyCrush.Core/Services/ServiceFactory.cs
@@ -0,0 +1,30 @@
+﻿using CandyCrush.Core.Data;
+using CandyCrush.Core.Services.Authentication;
+using CandyCrush.Core.Services.Communication;
+using CandyCrush.Core.Services.Game;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CandyCrush.Core.Services
+{
+    public static class ServiceFactory
+    {
+        public static void ConfigureServices(IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddScoped<CandyDbContext>();
+            services.AddScoped<IPasswordService, PasswordService>();
+            services.AddScoped<IAuthenticationService, AuthenticationService>();
+            services.AddScoped<IPlayerService, PlayerService>();
+            services.AddScoped<IFriendService, FriendService>();
+            services.AddScoped<IGameService, GameService>();
+            services.AddSingleton(configuration);
+
+        }
+    }
+}
---
+++ LoginSystem-WPF/CandyCrush.Core/Services/StaticLoggerService.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace CandyCrush.Core.Services
+{
+    public enum LogLevel
+    {
+        Info,
+        Warning,
+        Error
+    }
+
+    public static class Logger
+    {
+        private static string logFilePath = "./log.txt";
+
+        public static void Initialize(string filePath)
+        {
+            logFilePath = filePath;
+        }
+
+        public static void Log(LogLevel level, string message)
+        {
+            if (string.IsNullOrEmpty(logFilePath))
+            {
+                throw new InvalidOperationException("Logger has not been initialized. Call Initialize method before using the logger.");
+            }
+
+            string logEntry = $"{DateTime.Now} [{level.ToString().ToUpper()}] {message}";
+
+            StackTrace stackTrace = new();
+            StackFrame frame = stackTrace.GetFrame(1);
+            string caller = $"{frame.GetMethod().DeclaringType}.{frame.GetMethod().Name}";
+
+            logEntry += $" (Caller: {caller})";
+
+            using (StreamWriter writer = new(logFilePath, true))
+            {
+                writer.WriteLine(logEntry);
+            }
+
+            Console.WriteLine(logEntry);
+        }
+    }
+
+}
