94e3941637c5db14fc6edfc346a63d713280b147
Miaad <Miaad@MIAAD>
2023-06-27 10:20:01 +0330

    added session manager

---
+++ LoginSystem-WPF/CandyCrush.Core/Services/Authentication/SessionManager.cs
@@ -0,0 +1,28 @@
+﻿using CandyCrush.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CandyCrush.Core.Services.Authentication
+{
+    public class SessionManager
+    {
+        public static Session? CurrentSession { get; set; } = null;
+        public static bool IsCurrentSessionActive()
+        {
+            return CurrentSession != null &&
+                   !CurrentSession.HasExpired;
+        }
+
+        public static void AuthorizeMethodAccess(AccessLevels requiredAccessLevel)
+        {
+            if (requiredAccessLevel == AccessLevels.Unauthorized) return;
+            if (!IsCurrentSessionActive() || CurrentSession?.Player.AccessLevel < requiredAccessLevel)
+            {
+                throw new UnauthorizedAccessException("You are not authorized to access this functionality.");
+            }
+        }
+    }
+}
