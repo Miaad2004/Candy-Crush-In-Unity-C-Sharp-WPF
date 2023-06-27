f33272c687ddf14a24d3c8122eac5c9b761a4dbb
Miaad <Miaad@MIAAD>
2023-06-27 10:18:51 +0330

    added exceptions

---
+++ LoginSystem-WPF/CandyCrush.Core/Exceptions/UsernameAlreadyExistsException.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CandyCrush.Core.Exceptions
+{
+    public class UsernameAlreadyExistsException : ArgumentException
+    {
+        public UsernameAlreadyExistsException()
+        {
+        }
+
+        public UsernameAlreadyExistsException(string? username) : base($"Username ``{username}`` already exists.")
+        {
+        }
+
+        public UsernameAlreadyExistsException(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+
+        protected UsernameAlreadyExistsException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
