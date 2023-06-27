a3c8ac6057545fb790cca43869774dbd6da5b0fa
Miaad <Miaad@MIAAD>
2023-06-27 10:25:07 +0330

    added UserCard custom control

---
+++ LoginSystem-WPF/CandyCrush.Views/Controls/UserCard.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CandyCrush.UI.Controls
+{
+    /// <summary>
+    /// Interaction logic for UserCard.xaml
+    /// </summary>
+    public partial class UserCard : UserControl
+    {
+        public event EventHandler<UserEventArgs> AddFriendButtonClicked;
+
+        public UserCard()
+        {
+            InitializeComponent();
+        }
+
+        public static readonly DependencyProperty UsernameProperty = DependencyProperty.Register("Username", typeof(string), typeof(UserCard));
+        public string Username
+        {
+            get { return (string)GetValue(UsernameProperty); }
+            set { SetValue(UsernameProperty, value); }
+        }
+
+        public static readonly DependencyProperty LastSeenProperty = DependencyProperty.Register("LastSeen", typeof(string), typeof(UserCard));
+        public string LastSeen
+        {
+            get { return (string)GetValue(LastSeenProperty); }
+            set { SetValue(LastSeenProperty, value); }
+        }
+
+        public static readonly DependencyProperty ProfileImagePathProerty = DependencyProperty.Register("ProfileImagePath", typeof(string), typeof(UserCard));
+
+        public string ProfileImagePath
+        {
+            get { return (string)GetValue(ProfileImagePathProerty); }
+            set { SetValue(ProfileImagePathProerty, value); }
+        }
+
+        private void AddFriendButton_Click(object sender, RoutedEventArgs e)
+        {
+            AddFriendButtonClicked?.Invoke(this, new UserEventArgs(Username));
+        }
+    }
+}
---
+++ LoginSystem-WPF/CandyCrush.Views/Controls/UserEventArgs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CandyCrush.UI.Controls
+{
+    public class UserEventArgs : EventArgs
+    {
+        public string Username { get; }
+
+        public UserEventArgs(string username)
+        {
+            Username = username;
+        }
+    }
+}
