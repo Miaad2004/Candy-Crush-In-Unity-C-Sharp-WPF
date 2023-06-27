5286ad3e829ccda6c3355b95ed9b536e01a19920
Miaad <Miaad@MIAAD>
2023-06-27 10:26:43 +0330

    added MainWindow

---
+++ LoginSystem-WPF/CandyCrush.Views/MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using CandyCrush.Core.Services.Authentication;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CandyCrush.UI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly IAuthenticationService _authenticationService;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // Dependency Injection
+            var serviceProvider = (Application.Current as App ?? throw new ArgumentNullException(nameof(Application))).ServiceProvider;
+            _authenticationService = serviceProvider.GetRequiredService<IAuthenticationService>();
+
+            _ = MainFrame.NavigationService.Navigate(new Uri("Views/LoginView.xaml", UriKind.Relative));
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+        }
+
+        private void Minimize_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void Resize_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.WindowState == System.Windows.WindowState.Maximized)
+            {
+                this.WindowState = System.Windows.WindowState.Normal;
+            }
+
+            else
+            {
+                this.WindowState = System.Windows.WindowState.Maximized;
+            }
+        }
+
+        private void Close_Click(object sender, RoutedEventArgs e)
+        {
+            _authenticationService.Logout();
+            Application.Current.Shutdown();
+        }
+
+    }
+}
