7af50611f53f439cb274c7b94f490a925b54e4d6
Miaad <Miaad@MIAAD>
2023-06-27 10:21:42 +0330

    added PlayerService

---
+++ LoginSystem-WPF/CandyCrush.Core/Services/Communication/IPlayerService.cs
@@ -0,0 +1,14 @@
+﻿using CandyCrush.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CandyCrush.Core.Services.Communication
+{
+    public interface IPlayerService
+    {
+        Task<List<Player>> GetPlayers(int skipCount, int takeCount, string searchQuery="");
+    }
+}
---
+++ LoginSystem-WPF/CandyCrush.Core/Services/Communication/PlayerService.cs
@@ -0,0 +1,39 @@
+﻿using CandyCrush.Core.Data;
+using CandyCrush.Core.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CandyCrush.Core.Services.Communication
+{
+    public class PlayerService: IPlayerService
+    {
+        private readonly CandyDbContext dbContext;
+
+        public PlayerService(CandyDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public async Task<List<Player>> GetPlayers(int skipCount, int takeCount, string searchQuery = "")
+        {
+            IQueryable<Player> query = dbContext.Players;
+
+            if (!string.IsNullOrEmpty(searchQuery))
+            {
+                query = query.Where(p => p.Username.Contains(searchQuery));
+            }
+
+            List<Player> players = await query
+                .Skip(skipCount)
+                .Take(takeCount)
+                .ToListAsync();
+
+            return players;
+        }
+
+    }
+}
