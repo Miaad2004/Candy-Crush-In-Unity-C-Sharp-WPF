26be13988c636bdeffd15edffad3445fd0c4749f
Miaad <Miaad@MIAAD>
2023-06-27 10:19:45 +0330

    added password service

---
+++ LoginSystem-WPF/CandyCrush.Core/Services/Authentication/IPasswordService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CandyCrush.Core.Services.Authentication
+{
+    public interface IPasswordService
+    {
+        string GetHash(string? password);
+        bool IsStrong(string? password);
+        bool IsValid(string? password, string? passwordRepeat);
+    }
+}
